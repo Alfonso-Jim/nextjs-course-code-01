9b48303dbae209c526b5d45b457bdd3e6e462e0d
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-16 12:35:10 +0200

    Add nested navigation

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -1,7 +1,17 @@
 import { useRouter } from "next/router";
 
 const ClientsProjectsPage = () => {
-  return <div>Projects of Client</div>;
+  const router = useRouter();
+
+  const LoadProjectHandler = () => {
+    router.push({ pathname: "/clients/[id]/[clientProjectId]", query: { id: "punch", clientProjectId: "project1" } });
+  };
+  return (
+    <div>
+      Projects of Client
+      <button onClick={LoadProjectHandler}>Load project A</button>
+    </div>
+  );
 };
 
 export default ClientsProjectsPage;
