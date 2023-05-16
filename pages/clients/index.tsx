0ec2ccc47fec38dbcb69365ac3c9de9839e463f7
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-16 12:28:34 +0200

    Add dynamic link

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const ClientsPage = () => {
-  return <div>ClientsPage</div>;
+  const clients = [
+    {
+      id: "punch",
+      name: "Poncho",
+    },
+    {
+      id: "bash",
+      name: "Basia",
+    },
+  ];
+  return (
+    <div>
+      ClientsPage
+      <ul>
+        {clients.map((client) => (
+          <li>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ClientsPage;
