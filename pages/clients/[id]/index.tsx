ed6d4727024aa6cb7daa6e2a5fd1d7c0ffdd0f82
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-16 12:21:48 +0200

    remove clg

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -1,9 +1,6 @@
 import { useRouter } from "next/router";
-import React from "react";
 
 const ClientsProjectsPage = () => {
-  const router = useRouter();
-  console.log(router.query);
   return <div>Projects of Client</div>;
 };
 
