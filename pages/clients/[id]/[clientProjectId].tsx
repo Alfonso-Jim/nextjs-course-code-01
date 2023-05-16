c5d7bbf0f10c44e02799498a056708dbd48af9db
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-16 12:18:15 +0200

    remove clg

--- pages/clients/[id]/[clientProjectId].tsx
+++ pages/clients/[id]/[clientProjectId].tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 const SelectedClientProjectPage = () => {
   const router = useRouter();
-  console.log(router.query);
   return <div>Project page for specific project for a specific client</div>;
 };
 
